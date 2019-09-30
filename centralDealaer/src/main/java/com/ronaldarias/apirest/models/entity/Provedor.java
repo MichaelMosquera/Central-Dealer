b4285c5f9657c0f4cdca92716cc8a8eb1891ab64
juanManuelMontoya <50679743+juanManuelMontoya@users.noreply.github.com>
2019-09-30 13:25:10 -0500

    Se añade el ENTITY de provedor

---
+++ centralDealaer/src/main/java/com/ronaldarias/apirest/models/entity/Provedor.java
@@ -0,0 +1,79 @@
+package com.ronaldarias.apirest.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="proovedor")
+public class Provedor {
+	
+	@Id
+	@Column(name="nit")
+	private Integer nit;
+	
+	@Column(name="nombre")
+	private String nombre;
+	
+	@Column(name="direccion")
+	private String direccion;
+	
+	@Column(name="telefono")
+	private String telefono;
+	
+	@Column(name="estado")
+	private String estado;
+	
+	public Provedor() {
+		
+	}
+
+	public Integer getNit() {
+		return nit;
+	}
+
+	public void setNit(Integer nit) {
+		this.nit = nit;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		return "Provedor [nit=" + nit + ", nombre=" + nombre + ", direccion=" + direccion + ", telefono=" + telefono + ", estado="+ estado +"]";
+	}
+
+}
