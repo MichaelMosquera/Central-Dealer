1cf12a143135d422550bbf0b5d2f5002f564bbcf
MichaelMosquera <maikolstevens79@gmail.com>
2019-09-29 22:28:32 -0500

    init proyecto

--- centralDealaer/src/main/java/com/ronaldarias/apirest/models/entity/Tienda.java
+++ centralDealaer/src/main/java/com/ronaldarias/apirest/models/entity/Tienda.java
@@ -24,4 +24,49 @@ public class Tienda {
 	
 	@Column(name="emailtienda")
 	private String email;
+	
+	public String getNit() {
+		return nit;
+	}
+
+	public void setNit(String nit) {
+		this.nit = nit;
+	}
+
+	public String getNombreTienda() {
+		return nombreTienda;
+	}
+
+	public void setNombreTienda(String nombreTienda) {
+		this.nombreTienda = nombreTienda;
+	}
+
+	public String getDireccionTienda() {
+		return direccionTienda;
+	}
+
+	public void setDireccionTienda(String direccionTienda) {
+		this.direccionTienda = direccionTienda;
+	}
+
+	public String getIdDueño() {
+		return idDueño;
+	}
+
+	public void setIdDueño(String idDueño) {
+		this.idDueño = idDueño;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Tienda [nit=" + nit + ", nombre=" + nombreTienda + ", direccion=" + direccionTienda + ", dueño=" + idDueño + ", email=" + email + "]";
+	}
 }
